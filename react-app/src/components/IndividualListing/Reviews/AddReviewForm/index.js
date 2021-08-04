14a0b47246ad79b7ca0d321d8ed636bb6726b3f7
Rich-Supe <rsupe1993@aol.com>
2021-08-04 16:57:55 -0400

    added review create styling

--- react-app/src/components/IndividualListing/Reviews/AddReviewForm/index.js
+++ react-app/src/components/IndividualListing/Reviews/AddReviewForm/index.js
@@ -1,17 +1,53 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
+import { getListing } from '../../../../store/listing';
 
 import styles from './AddReviewForm.module.css'
+
+import { makeStyles } from '@material-ui/core/styles';
+import Rating from '@material-ui/lab/Rating';
+import Box from '@material-ui/core/Box';
 
 function AddReviewForm() {
     const user = useSelector(state => state.session.user);
     const dispatch = useDispatch();
     const history = useHistory();
+    const { listingId } = useParams();
+    const listing = useSelector( (state) => state.listing[listingId]);
+    console.log('listing from review form', listing)
 
-    const [ rating, setRating ] = useState(1);
+    // const [ rating, setRating ] = useState(1);
     const [ comment, setComment ] = useState('');
+    const [value, setValue] = useState(2);
+    const [hover, setHover] = useState(-1);
     
+    const labels = {
+        0.5: 'Terrible',
+        1: 'Bad',
+        1.5: 'Poor',
+        2: 'Meh',
+        2.5: 'Ok',
+        3: 'Decent',
+        3.5: 'Good',
+        4: 'Nice',
+        4.5: 'Excellent',
+        5: 'Perfect!',
+    };
+    
+    const useStyles = makeStyles({
+        root: {
+            width: 200,
+            display: 'flex',
+            alignItems: 'center',
+        },
+    });
+    
+    const classes = useStyles();
+
+    useEffect(() => {
+        dispatch(getListing(listingId));
+    }, [dispatch, listingId]);
 
     const onSubmit = async(e) => {
         e.preventDefualt();
@@ -29,19 +65,52 @@ function AddReviewForm() {
         // history.push("/listings/" + listing.id);
     };
 
-    return (
-        <div className={styles.ReviewFormPage}>
-            <form className={styles.form} onSubmit={onSubmit}>
-                <header>AddReviewForm</header>
-                <div className={styles.formBody}>
-                    <div className={styles.formRating}></div>
-                        <label htmlFor="rating">Rating</label>
-                        <input type="number" name="rating" id="rating" />
-                    </div>
+    if (listing) {
 
-            </form>
+    return (
+        <div className={styles.reviewFormPage}>
+            <div className={styles.formContainer}>
+                <form className={styles.form} onSubmit={onSubmit}>
+                    <h3 className={styles.header}>What do you think about {listing.name}?</h3>
+                    <div className={styles.formBody}>
+                        <div className={styles.formRating}>
+                            <div className={classes.root}>
+                                <Rating
+                                    name="hover-feedback"
+                                    value={value}
+                                    precision={0.5}
+                                    onChange={(event, newValue) => {
+                                    setValue(newValue);
+                                    }}
+                                    onChangeActive={(event, newHover) => {
+                                    setHover(newHover);
+                                    }}
+                                />
+                                {value !== null && <Box ml={2}>{labels[hover !== -1 ? hover : value]}</Box>}
+                            </div>
+                        </div>
+                        <div className={styles.formComment}>
+                            <label htmlFor="comment" className={styles.label}>Review here:</label>
+                            <textarea name="comment" id="comment" className={styles.area}/>
+                        </div>
+                    </div>
+                    <div className={styles.formFooter}>
+                        <button className={styles.button} type="submit">Submit</button>
+                        <button className={styles.return} onClick={() => history.push("/individual-listing/" + listing.id)}>Return</button>
+                    </div>
+                </form>
+            </div>
+            <div className={styles.imageContainer}>
+                <div className={styles.imageCard}>
+                    <img src={listing.images[0]?.image} alt="listing" className={styles.image}/>
+                </div>
+            </div>
+
         </div>
     )
+    } else {
+        return <div>Loading...</div>
+    }
 }
 
 export default AddReviewForm;
